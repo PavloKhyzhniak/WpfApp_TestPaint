18051b7484ba72aaf7be9614b62e7d14f6722184
Pavlo Khyzhniak <pavlokhyzhniak@gmail.com>
2022-03-10 00:15:11 +0300

    Add project files.

---
+++ WpfApp_TestPaint/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_TestPaint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+              
+        private void element_OnCornerMouseEnter(object sender, Cursor cursor)
+        {
+        }
+
+        // Ссылка на передвигаемый объект
+        UserControl movingElement = null;
+
+        // Координаты нажатия в передвигаемом объекте
+        Point elementCoords;
+
+        static int label_number = 0;
+
+        private void mainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (movingElement != null)
+            {
+                // Поместить отпущенный объект на самый нижний Z-уровень
+                Canvas.SetZIndex(movingElement, 0);
+
+                movingElement = null;
+            }
+        }
+
+        private void mainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                UserControl_Round.Unchecked();
+                return;
+            }
+            UserControl_Round userControl_Round = new UserControl_Round();
+
+            // Установить координаты для созданного элемента из координат мыши
+            Point coords = e.GetPosition(mainCanvas);
+            coords.Offset(-userControl_Round.Width / 2, -userControl_Round.Height / 2);
+
+            Canvas.SetLeft(userControl_Round, coords.X);
+            Canvas.SetTop(userControl_Round, coords.Y);
+
+            // Поместить созданный объект на самый нижний Z-уровень
+            Canvas.SetZIndex(userControl_Round, 0);
+
+            // Зарегистрировать обработчик нажатия мышью на созданном объекте
+            userControl_Round.MouseDown += Element_MouseDown;
+
+            userControl_Round.mainCanvas = mainCanvas;
+
+            mainCanvas.Children.Add(userControl_Round);
+        }
+
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (movingElement != null)
+            {
+                // Текущие координаты мыши на холсте
+                Point coords = e.GetPosition(mainCanvas);
+
+                // Перемещение элемента по новым координатам мыши, с учётом места нажатия на элементе
+                Canvas.SetLeft(movingElement, coords.X - elementCoords.X);
+                Canvas.SetTop(movingElement, coords.Y - elementCoords.Y);
+                return;
+            }
+            if (sender is Canvas)
+            {
+                e.Handled = false;
+                return;
+            }
+        }
+
+        private void Element_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            movingElement = (UserControl)sender;
+
+            // Получить координаты мыши внутри перемещаемого объекта
+            elementCoords = e.GetPosition(movingElement);
+
+            // Поместить выбранный объект на самый верхний Z-уровень
+            Canvas.SetZIndex(movingElement, 10);
+            e.Handled = true;
+        }
+
+    }
+}
---
+++ WpfApp_TestPaint/UserControl_Round.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_TestPaint
+{
+    public delegate void OnCornerMouseEnterDelegate(object sender, Cursor cursor);
+
+    /// <summary>
+    /// Interaction logic for UserControl_Round.xaml
+    /// </summary>
+    public partial class UserControl_Round : UserControl
+    {
+        // сообщение о нажатии мышью по одному из 
+        public event OnCornerMouseEnterDelegate OnCornerMouseEnter;
+
+        private static int count = 0;
+
+        private static int last_size = 150;
+        public Canvas mainCanvas { get; set; }
+        public UserControl_Round()
+        {
+            InitializeComponent();
+
+            this.textblock.Text = count.ToString();
+            count++;
+
+            // обработка изменения размера элемента управления
+            this.SizeChanged += Round_SizeChange;
+                        
+            this.MouseDown += UserControl_Round_MouseDown;
+            this.PreviewMouseDown += Corner_MouseDown;
+            this.PreviewMouseMove += Corner_MouseMove;
+
+            movingElement = null;
+            if (current != null)
+                Checked(current, false);
+            current = this;
+            Checked(this, false);
+        }
+
+        public static void Unchecked()
+        {
+            Checked(current, false);
+        }
+        private void Round_SizeChange(object sender, SizeChangedEventArgs e)
+        {
+            this.Width = last_size;
+            this.Height = last_size;
+
+            this.rectangle.Width = last_size;
+            this.rectangle.Height = last_size;
+            
+            this.elipse.Width = last_size;
+            this.elipse.Height = last_size;
+
+            this.textblock.FontSize = (int)(30 * last_size / 50);
+
+            int sizePoint = (int)(5 * last_size / 50);
+            this.elipseTopLeft.Width = sizePoint;
+            this.elipseTopLeft.Height = sizePoint;
+            this.elipseTopRight.Width = sizePoint;
+            this.elipseTopRight.Height = sizePoint;
+            this.elipseBottomLeft.Width = sizePoint;
+            this.elipseBottomLeft.Height = sizePoint;
+            this.elipseBottomRight.Width = sizePoint;
+            this.elipseBottomRight.Height = sizePoint;
+        }
+
+        private static UserControl_Round current;
+
+        private void Corner_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse el)
+            {
+                switch (el.Tag)
+                {
+                    case "TopLeft":
+                        el.Cursor = Cursors.ScrollSE;
+                        OnCornerMouseEnter?.Invoke(this, Cursors.ScrollSE);
+                        break;
+                    case "TopRight":
+                        el.Cursor = Cursors.ScrollSW;
+                        OnCornerMouseEnter?.Invoke(this, Cursors.ScrollSW);
+                        break;
+                    case "BottomLeft":
+                        el.Cursor = Cursors.ScrollNE;
+                        OnCornerMouseEnter?.Invoke(this, Cursors.ScrollNE);
+                        break;
+                    case "BottomRight":
+                        el.Cursor = Cursors.ScrollNW;
+                        OnCornerMouseEnter?.Invoke(this, Cursors.ScrollNW);
+                        break;
+                    default:
+                        el.Cursor = Cursors.AppStarting;
+                        OnCornerMouseEnter?.Invoke(this, Cursors.AppStarting);
+                        break;
+                }
+            }
+        }
+        // Координаты нажатия в передвигаемом объекте
+        Point elementCoords;
+        // Ссылка на передвигаемый объект
+        public static Ellipse movingElement = null;
+        private string corner;
+        private void Corner_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(sender is Ellipse el)
+            {
+                if (!el.Equals(movingElement))
+                {
+                    switch (el.Tag)
+                    {
+                        case "TopLeft":
+                        case "TopRight":
+                        case "BottomLeft":
+                        case "BottomRight":
+                            corner = (string)el.Tag;
+                            movingElement = el;
+
+                            // Получить координаты мыши внутри перемещаемого объекта
+                            elementCoords = e.GetPosition(mainCanvas);
+                            break;
+                        default:
+                            movingElement = null;
+                            break;
+                    }
+                    //                OnCornerMouseDown?.Invoke(this, (string)((Ellipse)sender).Tag);
+                    e.Handled = true;
+                    return;
+                }
+            }
+            movingElement = null;
+            e.Handled = false;
+        }
+        private void Corner_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                movingElement = null;
+                if (movingElement != null)
+            {
+                // Текущие координаты мыши на холсте
+                Point coords = e.GetPosition(mainCanvas);
+                int deltaX = (int)(coords.X - elementCoords.X);
+                int deltaY = (int)(coords.Y - elementCoords.Y);
+
+                switch (corner)
+                {
+                    case "TopLeft":
+                        last_size += Math.Abs(deltaX) > Math.Abs(deltaY) ? -deltaX : -deltaY;
+                        break;
+                    case "TopRight":
+                        last_size += Math.Abs(deltaX) > Math.Abs(deltaY) ? deltaX : -deltaY;
+                        break;
+                    case "BottomLeft":
+                        last_size += Math.Abs(deltaX) > Math.Abs(deltaY) ? -deltaX : deltaY;
+                        break;
+                    case "BottomRight":
+                        last_size += Math.Abs(deltaX) > Math.Abs(deltaY) ? deltaX : deltaY;
+                        break;
+                }
+//                last_size += Math.Abs(deltaX)> Math.Abs(deltaY) ? deltaX:deltaY;
+
+                if (last_size < 50)
+                    last_size = 50;
+                elementCoords = coords;
+                Round_SizeChange(sender, null);
+            }
+        }
+        private void UserControl_Round_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is UserControl_Round round)
+            {
+                last_size = (int)round.rectangle.Width;
+                if (current == null)
+                {
+                    current = round;
+                }
+                else if (!round.Equals(current))
+                {
+                    Checked(current, false);
+                    current = round;
+                }
+
+                Checked(current, true);
+
+            }    
+        }
+
+        private static void Checked(UserControl_Round current, bool v)
+        {
+            Visibility visibility = Visibility.Visible;
+            if (v)
+            {
+                current.elipseTopLeft.Visibility = visibility;
+                current.elipseTopRight.Visibility = visibility;
+                current.elipseBottomLeft.Visibility = visibility;
+                current.elipseBottomRight.Visibility = visibility;
+                current.rectangle.Visibility = visibility;
+            }
+            else
+            {
+                visibility = Visibility.Hidden;
+                current.elipseTopLeft.Visibility = visibility;
+                current.elipseTopRight.Visibility = visibility;
+                current.elipseBottomLeft.Visibility = visibility;
+                current.elipseBottomRight.Visibility = visibility;
+                current.rectangle.Visibility = visibility;
+            }
+        }
+
+    }
+}
